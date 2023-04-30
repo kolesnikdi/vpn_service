ea3c2959686afa5a71c4bac051a85d57d570a07d
kolesnikdi <kolesnik.d.i@gmail.com>
2023-04-30 15:46:51 +0300

    [menu] fixes

--- menu/views.py
+++ menu/views.py
@@ -1,26 +1,10 @@
-from rest_framework import viewsets, filters, generics, status
+from rest_framework import filters, generics, status
 
 from django_filters.rest_framework import DjangoFilterBackend
 from location.models import Location
 from menu.filters import CustomSearchFilter, CustomRangeFilter, perform_product_filtering
 from menu.serializers import LocationMenuSerializer, ProductMenuSerializer
-from product.models import Product
 from rest_framework.response import Response
-
-
-class ProductMenuView(viewsets.ReadOnlyModelViewSet):
-    queryset = Product.objects.all()
-    pagination_class = None
-    serializer_class = ProductMenuSerializer
-    filter_backends = [CustomSearchFilter, CustomRangeFilter, filters.OrderingFilter, DjangoFilterBackend]
-    filterset_fields = ['name', 'cost']
-    search_fields = ['name', 'description']
-    ordering_fields = ['name', 'cost', 'volume']
-
-    def get_queryset(self):
-        code = self.kwargs['code']
-
-        return Product.objects.filter(locations__code=code).order_by('id')
 
 
 class LocationMenuView(generics.RetrieveAPIView):
@@ -40,3 +24,18 @@ class LocationMenuView(generics.RetrieveAPIView):
         product_qs = perform_product_filtering(self, location_products_qs)
         result['products'] = ProductMenuSerializer(product_qs, many=True).data
         return Response(result, status=status.HTTP_200_OK)
+
+
+# class ProductMenuView(viewsets.ReadOnlyModelViewSet):
+#     """Version only for serializing products. Based on viewsets with turn off pagination"""
+#     queryset = Product.objects.all()
+#     pagination_class = None
+#     serializer_class = ProductMenuSerializer
+#     filter_backends = [CustomSearchFilter, CustomRangeFilter, filters.OrderingFilter, DjangoFilterBackend]
+#     filterset_fields = ['name', 'cost']
+#     search_fields = ['name', 'description']
+#     ordering_fields = ['name', 'cost', 'volume']
+#
+#     def get_queryset(self):
+#         code = self.kwargs['code']
+#         return Product.objects.filter(locations__code=code).order_by('id')
