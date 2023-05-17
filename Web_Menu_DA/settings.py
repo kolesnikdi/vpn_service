aacdb2731b4c13c36e3feb548ac8900b6b4a70f0
kolesnikdi <kolesnik.d.i@gmail.com>
2023-05-17 17:49:17 +0300

    [clear_docker_postgres] folder hierarchy

--- Web_Menu_DA/settings.py
+++ Web_Menu_DA/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("POSTGRES_DB", 'Postgres_Web_Menu_DA'),
         "USER": os.environ.get("POSTGRES_USER", 'postgres'),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", 'K@i160486'),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", 'postgres'),
         "HOST": os.environ.get("POSTGRES_HOST"),
         "PORT": os.environ.get("POSTGRES_PORT", "5433"),
     }
