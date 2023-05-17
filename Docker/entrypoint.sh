b2a476db232f0338b2ecb4db9f96996b634cc4f6
kolesnikdi <kolesnik.d.i@gmail.com>
2023-05-17 18:22:48 +0300

    [clear_docker_postgres] clean up

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -11,7 +11,6 @@ while ! nc -z $POSTGRES_HOST $POSTGRES_PORT;
 echo "PostgreSQL started"
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
-python manage.py shell -c "from registration.models import WebMenuUser; WebMenuUser.objects.create_superuser('segareta@ukr.net', 'segareta@ukr.net')"
 # python manage.py runserver 0.0.0.0:8000  --noreload # comand that stops reloading
 
 exec "$@"
